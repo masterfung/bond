c8b610da7291d2a08cbc0c3f387c7ad173ddcc5f
Tsung Hung <hungmasterj@gmail.com>
2014-08-18 15:03:36 -0700

    tasks updates

--- apps/meetup/tasks.py
+++ apps/meetup/tasks.py
@@ -1,4 +1,4 @@
-from settings import local
+# from settings import local
 
 __author__ = '@masterfung'
 import json
@@ -18,28 +18,28 @@ from apps.meetup.celery import app
 def hello_world():
     print('Hello World')
 
-MEETUP_API_KEY = local.MEETUP_API_KEY
-
-def meetup_api_find_open_events(request):
-    resp = get("https://api.meetup.com/2/open_events.json",
-        params={
-            "key": MEETUP_API_KEY,
-            "city": "san francisco",
-            "state": "ca",
-            "country": "us",  # "topic": "python",
-        },
-        )
-
-    if resp.status_code != 200:
-        print "error"
-        return
-
-    data = dumps(resp.json(), indent=2, sort_keys=True)
-
-    events = json.loads(data)
-
-    events = events['results']
-    print events
+# MEETUP_API_KEY = local.MEETUP_API_KEY
+#
+# def meetup_api_find_open_events(request):
+#     resp = get("https://api.meetup.com/2/open_events.json",
+#         params={
+#             "key": MEETUP_API_KEY,
+#             "city": "san francisco",
+#             "state": "ca",
+#             "country": "us",  # "topic": "python",
+#         },
+#         )
+#
+#     if resp.status_code != 200:
+#         print "error"
+#         return
+#
+#     data = dumps(resp.json(), indent=2, sort_keys=True)
+#
+#     events = json.loads(data)
+#
+#     events = events['results']
+#     print events
     # serialized_data = TopicEventSerializer(data=events, many=True)
     # if serialized_data.is_valid():
     #     serialized_data.save()
@@ -47,5 +47,5 @@ def meetup_api_find_open_events(request):
     #     print serialized_data.errors
 
 
-
-    return HttpResponse(data, content_type='application/json')
+    #
+    # return HttpResponse(data, content_type='application/json')
