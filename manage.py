5dfebb45510749eb1db4a2ca6cf62c8a34d6c26c
Tsung Hung <hungmasterj@gmail.com>
2014-08-18 14:41:23 -0700

    small change to bond to bondandme for heroku

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     # Load the Heroku environment.
     from herokuapp.env import load_env
-    load_env(__file__, "bond")
+    load_env(__file__, "bondandme")
 
     # os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
