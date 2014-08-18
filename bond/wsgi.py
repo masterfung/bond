fdec7df357e98d1561f1375608becca43633575e
Tsung Hung <hungmasterj@gmail.com>
2014-08-18 14:49:58 -0700

    switch back on the wsgi to bond

--- bond/wsgi.py
+++ bond/wsgi.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.insert(0, '/Users/htm/my-bond/mybond')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bondandme.settings")
+sys.path.insert(0, '/Users/htm/Documents/bond')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bond.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
