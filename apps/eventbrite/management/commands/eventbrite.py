4ad45db009ccead0f17bcc25565551e7c8891275
Tsung Hung <hungmasterj@gmail.com>
2014-08-21 00:31:34 -0700

    next row eventbrite

--- apps/eventbrite/management/commands/eventbrite.py
+++ apps/eventbrite/management/commands/eventbrite.py
@@ -17,8 +17,8 @@ class Command(BaseCommand):
 
         cities = [ # "san+francisco",
                   # "boston", "new+york", "houston",
-                   "los+angeles", "baltimore", "austin", "san+antonio", "nashville", "seattle", "philadelphia",
-                  # "columbus", "dallas", "denver", "salt+lake+city", "las+vegas", "washington", "kansas+city",
+                  # "los+angeles", "baltimore", "austin", "san+antonio", "nashville", "seattle", "philadelphia",
+                   "columbus", "dallas", "denver", "salt+lake+city", "las+vegas", "washington", "kansas+city",
                   # "minneapolis", "atlanta", "orlando", "richmond",
                   # "jacksonville", "charlotte", "milwaukee"
         ]
