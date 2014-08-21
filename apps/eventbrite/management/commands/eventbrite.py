0a1fcb9ae39bff07a628705c08e0c07746667435
Tsung Hung <hungmasterj@gmail.com>
2014-08-21 00:46:40 -0700

    last set of eventbrite

--- apps/eventbrite/management/commands/eventbrite.py
+++ apps/eventbrite/management/commands/eventbrite.py
@@ -19,8 +19,8 @@ class Command(BaseCommand):
                   # "boston", "new+york", "houston",
                   # "los+angeles", "baltimore", "austin", "san+antonio", "nashville", "seattle", "philadelphia",
                   # "columbus", "dallas", "denver", "salt+lake+city", "las+vegas", "washington", "kansas+city",
-                   "minneapolis", "atlanta", "orlando", "richmond",
-                  # "jacksonville", "charlotte", "milwaukee"
+                  # "minneapolis", "atlanta", "orlando", "richmond",
+                   "jacksonville", "charlotte", "milwaukee"
         ]
 
         while page < 50:
