be0f1dfc826e23ecb177ec741d0b3f2b7cd1b8fb
Tsung Hung <hungmasterj@gmail.com>
2014-08-21 00:25:23 -0700

    next round of cities

--- apps/eventbrite/management/commands/eventbrite.py
+++ apps/eventbrite/management/commands/eventbrite.py
@@ -16,8 +16,8 @@ class Command(BaseCommand):
         page = 0
 
         cities = [ # "san+francisco",
-                   "boston", "new+york", "houston",
-                  # "los+angeles", "baltimore", "austin", "san+antonio", "nashville", "seattle", "philadelphia",
+                  # "boston", "new+york", "houston",
+                   "los+angeles", "baltimore", "austin", "san+antonio", "nashville", "seattle", "philadelphia",
                   # "columbus", "dallas", "denver", "salt+lake+city", "las+vegas", "washington", "kansas+city",
                   # "minneapolis", "atlanta", "orlando", "richmond",
                   # "jacksonville", "charlotte", "milwaukee"
