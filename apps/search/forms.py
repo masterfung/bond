2fd5e1e23683ccb4a296df9b66970d76f1710e09
Jake <me@jh.gg>
2014-09-20 14:51:17 -0400

    Clean up some code!

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -2,8 +2,8 @@ from haystack.forms import SearchForm
 from django.utils import timezone
 
 
-class FutureModelSearchForm(SearchForm):
+class FutureSearchForm(SearchForm):
     def search(self):
-        sqs = super(FutureModelSearchForm, self).search()
+        sqs = super(FutureSearchForm, self).search()
         return sqs.filter(start_date__gte=timezone.now())
 
--- apps/search/views.py
+++
@@ -1,8 +0,0 @@
-from haystack.views import SearchView as HaystackSearchView
-
-from apps.search.forms import FutureModelSearchForm
-
-
-class SearchView(HaystackSearchView):
-    form = FutureModelSearchForm
-
