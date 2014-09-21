e393b711d6869581bf46b2e2b9eb56e5071a1341
Tsung Hung <hungmasterj@gmail.com>
2014-09-20 21:37:44 -0700

    centered the search and search field

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -1,8 +1,10 @@
 from haystack.forms import SearchForm
 from django.utils import timezone
+from apps.profiles.models import UserCity
 
 
 class FutureModelSearchForm(SearchForm):
+
     def search(self):
         sqs = super(FutureModelSearchForm, self).search()
         return sqs.filter(start_date__gte=timezone.now())
