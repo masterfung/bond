6423153498bf97737ab5a044cf823c82d755bbbb
Tsung Hung <hungmasterj@gmail.com>
2014-09-24 17:52:15 -0700

    deleted error on merge conflict

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -3,12 +3,7 @@ from django.utils import timezone
 from apps.profiles.models import UserCity
 
 
-<<<<<<< HEAD
-class FutureModelSearchForm(SearchForm):
-
-=======
 class FutureSearchForm(SearchForm):
->>>>>>> FETCH_HEAD
     def search(self):
         sqs = super(FutureSearchForm, self).search()
         return sqs.filter(start_date__gte=timezone.now())
