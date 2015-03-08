d819cc8b5f7f955e854711d3d5a5f112f30b909b
Tsung Hung <hungmasterj@gmail.com>
2015-03-07 21:54:30 -0800

    updated timezone to use datetime

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -1,12 +1,13 @@
 from haystack.forms import SearchForm
-from django.utils import timezone
+# from django.utils import timezone
 from apps.profiles.models import UserCity
+from datetime import datetime
 
 
 class FutureSearchForm(SearchForm):
     """Override the FutureSearchForm to show the dates that are greater than or equal to today.
     This insures events are most recent."""
     def search(self):
-        sqs = super(SearchForm, self).search()
-        return sqs.filter(start_date__gte=timezone.now())
+        sqs = super(FutureSearchForm, self).search()
+        return sqs.filter(start_date__gte=datetime.now())
 
